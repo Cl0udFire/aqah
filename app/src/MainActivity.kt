fa4f43be74ea2ceb4ec5d5a703c69f381ae1b739
Kwon Yuho <103117239+museekee@users.noreply.github.com>
2025-10-24 22:37:29 +0900

    web: router

---
+++ app/src/MainActivity.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package com.cloudfire.project1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.google.gms.googleservices.GoogleServicesPlugin
+
+
+@Composable
+fun Screen() {
+    MaterialTheme() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            BasicText("Hello, basic World!")
+        }
+    }
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Screen()
+        }
+    }
+}
---
+++ web/src/HomePage copy.jsx
@@ -0,0 +1,9 @@
+const HomePage2 = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homeaa Page</h1>
+    </div>
+  );
+};
+
+export default HomePage2;
