506aea3faa8a19f35b6fcd0eee73e9a8c4b6ea46
Kor-XCoder <mcoder1117@gmail.com>
2025-10-25 07:58:24 +0900

    web: Add sidebar

--- web/src/pages/HomePage.jsx
+++ web/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import app from "../firebase/firebase"
 import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import {useAppStore} from '../context/store'
-import {Sidebar} from "../components/Sidebar"
+import Sidebar from "../components/Sidebar"
 
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider();
@@ -30,25 +30,29 @@ const App = () => {
 
   return (
       <div className="container">
-          {!user && (
-              <img
-                  className="btn"
-                  onClick={() => handleLogin()}
-                  src="https://media.geeksforgeeks.org/wp-content/uploads/20240520175106/Google_SignIn_Logo.png"
-                  alt="NA"
-              />
-          )}
-          {user  && (
-              <>
-                  <div className="user">
-                      <h1>Your Successfully Loged In</h1>
-                      <h2>Name: {user.displayName}</h2>
-                      <h2>Email: {user.email}</h2>
-                      <img src={user.photoURL} alt="N/A" />
-                      <button onClick={() => handleLogout()} className="logout">Log Out</button>
-                  </div>
-              </>
-          )}
+        <Sidebar />
+        <div className="content" style={{ marginLeft: "250px" }}>
+            {!user && (
+                <img
+                    className="btn"
+                    onClick={() => handleLogin()}
+                    src="https://media.geeksforgeeks.org/wp-content/uploads/20240520175106/Google_SignIn_Logo.png"
+                    alt="NA"
+                />
+            )}
+            {user  && (
+                <>
+                    <div className="user">
+                        <h1>Your Successfully Loged In</h1>
+                        <h2>Name: {user.displayName}</h2>
+                        <h2>Email: {user.email}</h2>
+                        <img src={user.photoURL} alt="N/A" />
+                        <button onClick={() => handleLogout()} className="logout">Log Out</button>
+                    </div>
+                </>
+            )}
+        </div>
+          
       </div>
   );
 };
