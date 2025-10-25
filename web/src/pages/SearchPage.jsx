4673df170f1d1b90ff25314a8630d3df1e54127e
Kwon Yuho <103117239+museekee@users.noreply.github.com>
2025-10-25 15:21:48 +0900

    web: searchpage topbar

--- web/src/pages/SearchPage.jsx
+++ web/src/pages/SearchPage.jsx
@@ -1,10 +1,15 @@
 import { useLocation } from "react-router-dom";
+import type Topbar from "../components/Topbar";
 
 const SearchPage = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const query = queryParams.get("q");
-  return <div>Search Results for: {query}</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Topbar />
+    </div>
+  );
 };
 
 export default SearchPage;
