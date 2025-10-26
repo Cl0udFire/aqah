241f5b4c1fcfffb8a03c2b10b256747d9d91c4f9
Kor-XCoder <mcoder1117@gmail.com>
2025-10-26 10:18:22 +0900

    web: Tailwindcss v4 Update

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,5 @@
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-
 export default {
-  plugins: [autoprefixer, tailwindcss],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
