83c8d87ad82f42ec9582bc268acd98975440e00c
Kwon Yuho <103117239+museekee@users.noreply.github.com>
2025-10-26 13:44:56 +0900

    web: 랭킹 데이터 변경

--- web/src/pages/RankingPage.jsx
+++ web/src/pages/RankingPage.jsx
@@ -72,33 +72,27 @@ const RankingPage = () => {
           {[
             {
               rank: 1,
-              name: "UserA",
-              exp: 1500,
+              name: "1428 주승민",
+              exp: 22397,
+              pfp: "https://images-ext-1.discordapp.net/external/0M8Y_wF_SNzIChYL8BW1Y1RvOOzFJwviV6VT8CvhZxY/%3Fsz%3D46/https/lh3.googleusercontent.com/-JxyS--23TxQ/AAAAAAAAAAI/AAAAAAAAAAA/ALKGfkmaXzlGED5P26sP9uNciFwKJ57Pqw/photo.jpg?format=webp&width=69&height=69",
             },
             {
               rank: 2,
-              name: "UserB",
-              exp: 1200,
+              name: "1402 김강민",
+              exp: 17618,
+              pfp: "https://images-ext-1.discordapp.net/external/o6gb9GubgAS391_BnRX75EjYwN0vjP8hH7nHAnoowug/https/lh3.googleusercontent.com/a/ACg8ocKR2FjGHISkJdc7qu5Cm3IzUhBFhC2RzuMi-PYeLAm2Fshu1enFGw%3Ds192-c?format=webp&width=288&height=288",
             },
             {
               rank: 3,
-              name: "UserC",
-              exp: 900,
+              name: "1401 권유호",
+              exp: 3264,
+              pfp: "https://lh3.googleusercontent.com/a/ACg8ocIHJi3xVpc7S49xJb_gu9QwaCT_v3UQBAS8jrSZOFxgQ313uR0=s96-c",
             },
             {
               rank: 4,
-              name: "UserD",
+              name: "1418 이재현",
               exp: 850,
-            },
-            {
-              rank: 5,
-              name: "UserE",
-              exp: 800,
-            },
-            {
-              rank: 6,
-              name: "UserF",
-              exp: 750,
+              pfp: "https://media.discordapp.net/attachments/1410402754186444810/1431863002537136189/ACg8ocIOVgPPiXIlSSkpFzXBNz4PcAcy9XP0YzQepbOEe9BhT6a7UQs504-c-mo-no.png?ex=68fef5d6&is=68fda456&hm=91cf5b366b1ff8534eefc5730e6e598019c221f517e00f5bf61f06ab09d343c3&=&format=webp&quality=lossless&width=756&height=756",
             },
           ]
             .sort((a, b) => b.exp - a.exp)
@@ -107,7 +101,11 @@ const RankingPage = () => {
                 <td className="text-right">{user.rank}</td>
                 <td className="flex items-center">
                   <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png"
+                    className="rounded-4xl"
+                    src={
+                      user.pfp ??
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png"
+                    }
                     alt={`${user.name}'s avatar`}
                   />
                   {user.name}
